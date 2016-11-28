9d04f9d45857cdac1521edff3d3cfa36ec348647
Alexander Logvinenko <lexdevel@gmail.com>
2016-11-28 14:56:46 +0200

    Implemented minification.

--- source/util/GameTime.ts
+++ source/util/GameTime.ts
@@ -1,6 +1,20 @@
+/**
+ * The game time class.
+ */
 class GameTime {
     public static initial: number = Date.now();
 
+    /**
+     * Constructor.
+     * @throws Always throws an exception because of static class.
+     */
+    constructor() {
+        throw new Error("Allowed static usage of class only");
+    }
+
+    /**
+     * Get the elapsed game time.
+     */
     public static elapsed(): number {
         return Date.now() - GameTime.initial;
     }
