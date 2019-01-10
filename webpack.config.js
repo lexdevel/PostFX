0d83437c3c6eea96f7a3ca9aad6b8cfbab0ff9b3
Alexander Logvinenko <lexdevel@gmail.com>
2019-01-10 13:06:07 +0200

    Upgraded webpack and other dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,23 @@
-var webpack = require("webpack");
-
 module.exports = {
-    "entry": "./source/PostFX",
-    "output": {
-        "filename": "postfx.bundle.js"
-    },
-    "resolve": {
-        "extensions": [
-            ".ts",
-            ".js"
-        ]
-    },
-    "module": {
-        "rules": [
-            {
-                "test": /\.ts$/,
-                "use": [
-                    {
-                        "loader": "ts-loader"
-                    }
-                ]
-            }
-        ]
-    },
-    "plugins": [
-        new webpack.optimize.UglifyJsPlugin({ "minimize": true })
+  "entry": "./src/PostFX",
+  "output": {
+    "filename": "postfx.bundle.js"
+  },
+  "resolve": {
+    "extensions": [
+      ".ts"
     ]
+  },
+  "module": {
+    "rules": [
+      {
+        "test": /\.ts$/,
+        "use": [
+          {
+            "loader": "ts-loader"
+          }
+        ]
+      }
+    ]
+  }
 };
